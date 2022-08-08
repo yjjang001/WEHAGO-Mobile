5b82d04bb2bbfdcc55311dc08632c7aaec63f46f
yjjang001 <109577053+yjjang001@users.noreply.github.com>
2022-08-08 15:15:42 +0900

    Add files via upload

---
+++ name.py
@@ -0,0 +1,34 @@
+# 공통
+listbar = "com.duzon.android.lulubizpotal:id/ib_main_toolbar_drawer"
+
+# 초기화면
+confirmbtn = "com.duzon.android.lulubizpotal:id/tv_confirm_buttom"
+allowbtn = "com.android.permissioncontroller:id/permission_allow_button"
+denybtn = "com.android.permissioncontroller:id/permission_deny_button"
+
+# 로그인
+loginId      = "com.duzon.android.lulubizpotal:id/et_login_insert_id"
+loginPw      = "com.duzon.android.lulubizpotal:id/et_login_insert_pw"
+loginButton  = "com.duzon.android.lulubizpotal:id/btn_login"
+loginError   = "com.duzon.android.lulubizpotal:id/tv_login_alert_error"
+
+# 시작하기
+startbtn = "com.duzon.android.lulubizpotal:id/btn_start"
+
+# 거래처관리
+enterAccounts    = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[8]/android.widget.RelativeLayout"
+returnAccounts    = "com.duzon.android.lulubizpotal:id/ib_main_toolbar_left"
+plusBtnAccounts     = "com.duzon.android.lulubizpotal:id/fam_partner_list_floating_action_menu"
+corporationAccounts   = "com.duzon.android.lulubizpotal:id/partner_add_menu_corporate"
+accountsName = "com.duzon.android.lulubizpotal:id/partner_edit_basic_name"
+enterpriseNumber    = "com.duzon.android.lulubizpotal:id/partner_edit_basic_company_reg_no"
+representativeName  = "com.duzon.android.lulubizpotal:id/partner_edit_basic_ceo_name"
+checkBtn    = "com.duzon.android.lulubizpotal:id/btn_right1"
+searchAddress    = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[4]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.TextView"
+accountGroup    = "com.duzon.android.lulubizpotal:id/btn_partner_edit_basic_group_name"
+selectAccounts  = "com.duzon.android.lulubizpotal:id/iv_dz_DropDownSelectBar_partner_icon_modify"
+deleteAccounts  = "com.duzon.android.lulubizpotal:id/ll_partner_list_modify_delete"
+allAccounts = "com.duzon.android.lulubizpotal:id/tv_dz_DropDownSelectBar_partner_selected_name"
+searchAccounts  = "com.duzon.android.lulubizpotal:id/common_search_bar_text"
+detailbar   = "com.duzon.android.lulubizpotal:id/btn_partner_text_tooblar_more"
+
